863a1d8f45842780e408873540cadfe86de67ebb
adarshkumar00 <adholakpur20@gmail.com>
2020-05-29 15:47:39 +0530

    working of up button

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './Details.css';
 
-
+let h=1;
 class Details extends Component {
     constructor() {
         super();
@@ -19,13 +19,29 @@ class Details extends Component {
                 email: "art@kj.com"
 
             }
+            ,
+            {
+                id:3,
+                name: "RAm",
+                phone: "5455204444",
+                email: "ythj@kj.com"
+
+            }
             ]
 
         }
     }
     onClickHandler=(e)=>{
+        
         console.log(e);
-        //document.getElementById("e").style.display="block";
+        document.getElementById(e).style.display="block";
+        console.log(e);
+    }
+    onClickHandler2=(e)=>{
+        
+        console.log(e);
+        document.getElementById(e).style.display="none";
+        console.log(e);
     }
 
     render() {
@@ -35,15 +51,16 @@ class Details extends Component {
                     <h1>Phone book</h1>
                     <div>
                         {this.state.consumerdetails.map((ele) => {
-                            let h=1;
+                            
                             let g="inner"+h;
+                            h=h+1;
                             return (
                                 <div id={ele.id}>
-                                    <div className="labelname">{ele.name}  <span><i className="arrow down" onClick={this.onClickHandler(g)}></i></span></div><br />
-                                    <div >
+                                    <div className="labelname">{ele.name}  <span><i className="arrow down" onClick={this.onClickHandler.bind(this,g)}></i></span></div><br />
+                                    <div id={g} className="hidden" >
                                         <label><i class="fa fa-phone"></i>{ele.phone}  <span><i className="fas fa-envelope-square"></i>{ele.email}
                                              
-                                        </span> <span> <i class="arrow up"></i></span>
+                                        </span> <span> <i class="arrow up"  onClick={this.onClickHandler2.bind(this,g)}></i></span>
 </label><br/>
                                     </div>
                                 </div>
